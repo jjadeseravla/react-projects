ebce6c549b46ccf2f639b89ca24f8f4220c3573a
Jade Alvares <jadea@webother.com>
2024-03-01 10:35:02 +0000

    dynamic query functions and keys

--- react-24-query/src/components/Events/NewEventsSection.jsx
+++ react-24-query/src/components/Events/NewEventsSection.jsx
@@ -10,7 +10,12 @@ export default function NewEventsSection() {
     // queryKey cashes data so data can be shown quicker to the 
     // user as doesnt need to be fetched all the time
     queryKey: ['events'],
-    queryFn: fetchEvents
+    queryFn: fetchEvents,
+    // 20 miliS of waiting before sending new req for new data
+    // to make sure no unneessary req made, the default is 0
+    staleTime: 0,
+    // gc is amount of time to keep cashed data before discarding it
+    // gcTime: 1000
   })
 
   let content;
